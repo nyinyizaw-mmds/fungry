06efa1a3fad0806d7eb704d07e8f41434e8d3468
kyaw-soe-aung <dev.kyawsoeaung@gmail.com>
2019-10-10 10:42:57 +0630

    to combine

---
+++ resources/views/partials/nav.blade.php
@@ -0,0 +1,15 @@
+<div class="header">
+    <div class="logo1">   
+            <img src="{{ ('images/logo1.png') }}" alt="logo"><p>Fungry.AI</p>  
+    </div>
+    {{-- <h4>Fungry AI</h4> --}}
+        <nav>
+             <ul>
+                 <li><a href="#">Home</a></li>
+                 <li><a href="#">About</a></li>
+                 <li><a href="#">Features</a></li>
+                 <li><a href="#">How It Works</a></li>
+                 <li><button class="btn1">Get the App</button></li>
+             </ul>
+         </nav> 
+</div>
