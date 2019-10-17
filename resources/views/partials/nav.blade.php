ca2eac0666fd47ab8f69c67bd65c3cc122bc9568
nyinyizaw-mmds <nyinyi.zaw@mm-digital-solutions.com>
2019-10-17 14:31:55 +0630

    new animation

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -6,7 +6,7 @@
     
         <nav>
              <ul>
-                 <li><a href="#home">Home</a></li>
+                 <li  class="home"><a href="#home">Home</a></li>
                  <li><a href="#about">About</a></li>
                  <li><a href="#three">Features</a></li>
                  <li><a href="#special">How It Works</a></li>
