559f3dfd9dc0d386aaa658cb30b53ac156180c70
nyinyizaw-mmds <nyinyi.zaw@mm-digital-solutions.com>
2019-10-18 13:49:19 +0630

    snippet

--- resources/views/partials/five.blade.php
+++ resources/views/partials/five.blade.php
@@ -13,6 +13,7 @@
                 </div>
                 <div class="slide_group">
                     <div class="card">
+                        <div class="transparent_cover"></div>
                         <img src="{{asset('/images/burger-chips-dinner-70497.jpg')}}" alt="phone" class="phimg2">
                         <i class="far fa-heart" ><span>12</span></i>
                         <div class="container">
@@ -23,6 +24,7 @@
                     </div>
         
                     <div class="card_1">
+                        <div class="transparent_cover"></div>
                         <img src="{{asset('/images/asian-food-bowl-cuisine-699953.jpg')}}" alt="phone" class="phimg2">
                         <i class="far fa-heart" ><span>12</span></i> 
                         <div class="container">
@@ -32,6 +34,7 @@
                     </div>
                     
                     <div class="card_2">
+                        <div class="transparent_cover"></div>
                         <img src="{{asset('/images/appetizer-avocado-bread-566566.jpg')}}" alt="phone" class="phimg2">
                         <i class="far fa-heart" ><span>12</span></i> 
                         <div class="container">
